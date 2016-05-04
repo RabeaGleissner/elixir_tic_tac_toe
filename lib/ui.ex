210a9265c2d509b356bbd8c41a21d71b51658ab7
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-04 15:37:38 +0100

    asks user again on invalid input

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,6 +1,38 @@
 defmodule Ui do
-def ask_for_position do
-    IO.puts "Please choose a position: "
-    IO.gets("")
+  def ask_for_position do
+    IO.puts "Please choose a position:"
+    get_users_position
+  end
+
+  def get_users_position do
+    input = clean_input(IO.gets(""))
+    if valid?(input) do
+      convert_to_integer(input)
+    else
+      ask_for_position
+    end
+  end
+
+  def clean_input(input) do
+    String.strip(input)
+  end
+
+  def valid?(input) do
+    valid_numbers = [1,2,3,4,5,6,7,8,9]
+    if is_number?(input) do
+      Enum.find(valid_numbers, false, fn(num) ->
+        num == convert_to_integer(input)
+      end)
+    else
+      false
+    end
+  end
+
+ defp convert_to_integer(string) do
+    String.to_integer(string)
+  end
+
+  defp is_number?(input) do
+    Integer.parse(input) != :error
   end
 end
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -3,8 +3,20 @@ defmodule UiTest do
   import ExUnit.CaptureIO
 
   test "it asks the user to enter a position" do
-    assert capture_io(fn ->
+    assert capture_io([input: "4\n"], fn ->
       Ui.ask_for_position
-    end) == "Please choose a position: \n"
+    end) == "Please choose a position:\n"
+  end
+
+  test "it returns the user's choice for a position" do
+     capture_io([input: "4\n"], fn ->
+       assert Ui.get_users_position == 4
+    end)
+  end
+
+  test "it asks for a position again if user input is invalid" do
+     assert capture_io([input: "n\n4\n"], fn ->
+       Ui.ask_for_position
+    end) == "Please choose a position:\nPlease choose a position:\n"
   end
 end
