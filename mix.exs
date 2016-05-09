1c2bb816ef376fa45f533728b13f0ccb5b241c87
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-09 15:44:46 +0100

    adds code coverage generator ExCoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule TicTacToe.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+    preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      deps: deps]
   end
 
@@ -27,6 +29,6 @@ defmodule TicTacToe.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:excoveralls, "~> 0.5", only: :test}]
   end
 end
