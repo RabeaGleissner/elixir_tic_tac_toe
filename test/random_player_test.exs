47a45f5c14436ca4909acd8178d341771c83f9fb
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-09 13:24:56 +0100

    random player returns just board instead of tuple

--- test/random_player_test.exs
+++ test/random_player_test.exs
@@ -4,6 +4,6 @@ defmodule RandomPlayerTest do
   test "it makes a random move on the board" do
     board = ["X","O","X",4,5,6,7,8,9]
     :rand.seed(:exsplus, {1, 2, 3})
-    assert {:ok, ["X", "O", "X", 4, 5, 6, 7, "O", 9]} == RandomPlayer.make_move(board)
+    assert ["X", "O", "X", 4, 5, 6, 7, "O", 9] == RandomPlayer.make_move(board)
   end
 end
