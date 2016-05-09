1a98178349fcd4fe2e9a5481b8ac42697705c671
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-09 13:31:21 +0100

    adds player factory

---
+++ lib/player_factor.ex
@@ -0,0 +1,10 @@
+defmodule PlayerFactory do
+
+  def players(game_mode) do
+    case game_mode do
+      :human_vs_human -> {HumanPlayer, HumanPlayer}
+      :human_vs_random -> {HumanPlayer, RandomPlayer}
+      :random_vs_human -> {RandomPlayer, HumanPlayer}
+    end
+  end
+end
