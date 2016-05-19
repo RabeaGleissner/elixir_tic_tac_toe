896f4976e5b870d669d906ef6cdb240b5fc9114a
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-19 14:44:45 +0100

    adds alpha beta pruning

--- lib/unbeatable_player.ex
+++ lib/unbeatable_player.ex
@@ -1,51 +1,70 @@
 defmodule UnbeatablePlayer do
+  @initial_alpha -10000
+  @initial_beta 10000
 
   def make_move(board), do: make_move(board, Board.next_player_mark(board))
   def make_move(board, computer_mark) do
-    [_, move] = minimax(board, computer_mark, computer_mark, 9)
+    [{_, move}, {_,_}] = minimax(board, computer_mark, computer_mark, 9, {@initial_alpha, @initial_beta})
     move
     #Board.place_mark(board, move)
   end
 
-  def minimax(current_board, computer_mark, current_mark, depth) do
+  def minimax(current_board, computer_mark, current_mark, depth, alpha_beta) do
     current_board
     |> finish_calculation?(depth)
-    |> result(current_board, computer_mark, current_mark, depth)
+    |> result(current_board, computer_mark, current_mark, depth, alpha_beta)
   end
 
   defp finish_calculation?(board, depth), do: if Board.game_over?(board) || depth == 0, do: :finish, else: :continue
 
-  defp result(:finish, board, computer_mark, _, depth), do: [score(board, computer_mark, depth), -1]
-  defp result(:continue, board, computer_mark, current_mark, depth) do
+  defp result(:finish, board, computer_mark, _, depth, alpha_beta), do: [{score(board, computer_mark, depth), -1}, alpha_beta]
+  defp result(:continue, board, computer_mark, current_mark, depth, alpha_beta) do
     board
     |> Board.available_positions
-    |> Enum.reduce([initialize_score(current_mark, computer_mark), -1], fn(next_move), [score, position] ->
-      evaluate_game_states(board, next_move, computer_mark, current_mark, depth, score, position)
+    |> Enum.reduce_while([{starting_score(current_mark, computer_mark), -1}, alpha_beta], fn(next_move), result_accumulator ->
+      evaluate_game_states(board, next_move, computer_mark, current_mark, depth, result_accumulator)
     end)
   end
 
-  defp evaluate_game_states(board, next_move, computer_mark, current_mark, depth, score, position) do
+  defp evaluate_game_states(board, next_move, computer_mark, current_mark, depth, [{score, position}, alpha_beta]) do
     board
     |> Board.place_mark(next_move)
-    |> minimax(computer_mark, switch(current_mark), depth - 1)
+    |> minimax(computer_mark, switch(current_mark), depth - 1, alpha_beta)
     |> find_best_scored_move([score, position], [current_mark, computer_mark], next_move)
+    |> update_alpha_beta(computer_mark, current_mark)
+    |> break_or_continue
   end
 
-  defp find_best_scored_move([current_score, _], [best_score, best_position], [current_mark, computer_mark], next_move) do
+  defp update_alpha_beta([{score, move}, {alpha, beta}], computer_mark, current_mark) do
+    new_alpha =
+    #if computer_mark == current_mark, do: Enum.max([alpha, score]), else: alpha
+    alpha
+
+    new_beta =
+    #if computer_mark != current_mark, do: Enum.min([beta, score]), else: beta
+    beta
+
+    [{score, move}, {new_alpha, new_beta}]
+  end
+
+  defp break_or_continue([{score, move}, {alpha, beta}]) when alpha >= beta, do: {:halt, [{score, move}, {alpha, beta}]}
+  defp break_or_continue([{score, move}, {alpha, beta}]), do: {:cont, [{score, move}, {alpha, beta}]}
+
+  defp find_best_scored_move([{current_score, _}, alpha_beta], [best_score, best_position], [current_mark, computer_mark], next_move) do
     computer_mark
     |> current_player(current_mark)
-    |> score_better(current_score, best_score)
-    |> update_score([current_score, next_move], [best_score, best_position])
+    |> better_score(current_score, best_score)
+    |> update_score({current_score, next_move}, {best_score, best_position}, alpha_beta)
   end
 
   defp current_player(mark, mark), do: :computer
   defp current_player(_, _), do: :opponent
 
-  defp score_better(:computer, current_score, best_score), do: current_score > best_score
-  defp score_better(:opponent, current_score, best_score), do: current_score < best_score
+  defp better_score(:computer, current_score, best_score), do: current_score > best_score
+  defp better_score(:opponent, current_score, best_score), do: current_score < best_score
 
-  defp update_score(true, updated_scored_position, _), do: updated_scored_position
-  defp update_score(false, _, best_scored_position), do: best_scored_position
+  defp update_score(true, updated_scored_position, _, alpha_beta), do: [updated_scored_position, alpha_beta]
+  defp update_score(false, _, best_scored_position, alpha_beta), do: [best_scored_position, alpha_beta]
 
   def score(board, computer_mark, depth) do
     board
@@ -64,7 +83,7 @@ defmodule UnbeatablePlayer do
 
   defp switch(mark), do: if (mark == "X"), do: "O", else: "X"
 
-  defp initialize_score(current_mark, computer_mark) do
+  defp starting_score(current_mark, computer_mark) do
     if (current_mark == computer_mark), do: -1000, else: 1000
   end
 end
