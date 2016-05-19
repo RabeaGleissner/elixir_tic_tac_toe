feaf5bfb991955bb4fabde29fc5387eb3d995a39
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-19 14:57:21 +0100

    clusters parameters to reduce amount

--- lib/unbeatable_player.ex
+++ lib/unbeatable_player.ex
@@ -4,21 +4,21 @@ defmodule UnbeatablePlayer do
 
   def make_move(board), do: make_move(board, Board.next_player_mark(board))
   def make_move(board, computer_mark) do
-    [{_, move}, {_,_}] = minimax(board, computer_mark, computer_mark, 9, {@initial_alpha, @initial_beta})
+    [{_, move}, {_,_}] = minimax(board, {computer_mark, computer_mark}, 9, {@initial_alpha, @initial_beta})
     move
     #Board.place_mark(board, move)
   end
 
-  def minimax(current_board, computer_mark, current_mark, depth, alpha_beta) do
+  def minimax(current_board, marks, depth, alpha_beta) do
     current_board
     |> finish_calculation?(depth)
-    |> result(current_board, computer_mark, current_mark, depth, alpha_beta)
+    |> result(current_board, marks, depth, alpha_beta)
   end
 
   defp finish_calculation?(board, depth), do: if Board.game_over?(board) || depth == 0, do: :finish, else: :continue
 
-  defp result(:finish, board, computer_mark, _, depth, alpha_beta), do: [{score(board, computer_mark, depth), -1}, alpha_beta]
-  defp result(:continue, board, computer_mark, current_mark, depth, alpha_beta) do
+  defp result(:finish, board, {computer_mark, _}, depth, alpha_beta), do: [{score(board, computer_mark, depth), -1}, alpha_beta]
+  defp result(:continue, board, {computer_mark, current_mark}, depth, alpha_beta) do
     board
     |> Board.available_positions
     |> Enum.reduce_while([{starting_score(current_mark, computer_mark), -1}, alpha_beta], fn(next_move), result_accumulator ->
@@ -29,36 +29,33 @@ defmodule UnbeatablePlayer do
   defp evaluate_game_states(board, next_move, computer_mark, current_mark, depth, [{score, position}, alpha_beta]) do
     board
     |> Board.place_mark(next_move)
-    |> minimax(computer_mark, switch(current_mark), depth - 1, alpha_beta)
-    |> find_best_scored_move([score, position], [current_mark, computer_mark], next_move)
+    |> minimax({computer_mark, switch(current_mark)}, depth - 1, alpha_beta)
+    |> find_best_scored_move([score, position], {current_mark, computer_mark}, next_move)
     |> update_alpha_beta(computer_mark, current_mark)
     |> break_or_continue
   end
 
   defp update_alpha_beta([{score, move}, {alpha, beta}], computer_mark, current_mark) do
-    new_alpha =
-    #if computer_mark == current_mark, do: Enum.max([alpha, score]), else: alpha
-    alpha
-
-    new_beta =
-    #if computer_mark != current_mark, do: Enum.min([beta, score]), else: beta
-    beta
+    #new_alpha = if computer_mark == current_mark, do: Enum.max([alpha, score]), else: alpha
+    #new_beta = if computer_mark != current_mark, do: Enum.min([beta, score]), else: beta
+    new_alpha = alpha
+    new_beta = beta
 
     [{score, move}, {new_alpha, new_beta}]
   end
 
-  defp break_or_continue([{score, move}, {alpha, beta}]) when alpha >= beta, do: {:halt, [{score, move}, {alpha, beta}]}
-  defp break_or_continue([{score, move}, {alpha, beta}]), do: {:cont, [{score, move}, {alpha, beta}]}
+  defp break_or_continue([score_move, {alpha, beta}]) when alpha >= beta, do: {:halt, [score_move, {alpha, beta}]}
+  defp break_or_continue([score_move, alpha_beta]), do: {:cont, [score_move, alpha_beta]}
 
-  defp find_best_scored_move([{current_score, _}, alpha_beta], [best_score, best_position], [current_mark, computer_mark], next_move) do
-    computer_mark
-    |> current_player(current_mark)
+  defp find_best_scored_move([{current_score, _}, alpha_beta], [best_score, best_position], marks, next_move) do
+    marks
+    |> current_player
     |> better_score(current_score, best_score)
     |> update_score({current_score, next_move}, {best_score, best_position}, alpha_beta)
   end
 
-  defp current_player(mark, mark), do: :computer
-  defp current_player(_, _), do: :opponent
+  defp current_player({mark, mark}), do: :computer
+  defp current_player({_, _}), do: :opponent
 
   defp better_score(:computer, current_score, best_score), do: current_score > best_score
   defp better_score(:opponent, current_score, best_score), do: current_score < best_score
